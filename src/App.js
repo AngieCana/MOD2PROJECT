34bcc97690b74178c991c0bc92415b98810c7076
AngieCana <grilozangie@gmail.com>
2023-07-27 22:40:54 -0400

    first commit: decided to go with a accordion faq about OSU. Scrapped idea of extra faq section. Will be adding correct images to next commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Accordion from './components/Accordion';
+import Banner from "./components/Banner"
+import {accordionData} from "./utils/content"
+import Carousel from './components/carousel';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ohio State Football FAQ</h1>
+      <div className="accordion">
+        <Banner/>
+        <Carousel/>
+        {accordionData.map(({ title, content }) => (
+          <Accordion title={title} content={content}/>
+          
+        
+         ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Banner() {
+  return (
+    <div>
+        <h2>
+            Ohio State Team Fan Page
+        </h2>
+    </div>
+  )
+}
+
